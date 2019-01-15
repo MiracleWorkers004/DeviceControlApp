4b7551c96dcccdf7937eec5142b24f5b757d2c32
Audiology <audiology@Audiologys-MacBook-Pro.local>
2019-01-15 10:10:24 +0530

    modified unitest1 file

--- DemoUnitTest/UnitTest1.cs
+++ DemoUnitTest/UnitTest1.cs
@@ -1,5 +1,9 @@
 using DeviceControlApp.Model;
+using System.Threading.Tasks;
+
+using DeviceControlApp.ViewModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace DemoUnitTest
 {
@@ -11,10 +15,46 @@ namespace DemoUnitTest
         {
 
             var demo = new DemoDate();
-            demo.Message = "Hi1s";
+            demo.Message = "Hi";
             Assert.AreEqual("Hi", demo.Message);
-
-            Assert.Fail("hi","hi");
         }
+
+
+        [TestMethod]
+        public void When_we_go_next_in_home_then_we_go_to_products_page()
+        {
+            var dummyPageService = new DummyPageService();
+            var homePageViewModel = new HomePageViewModel(dummyPageService);
+            var canGoNext = homePageViewModel.NextCommand.CanExecute(null);
+            homePageViewModel.NextCommand.Execute(null);
+
+            Assert.AreEqual(true, canGoNext);
+            Assert.AreEqual(typeof(ProductViewModel), dummyPageService.GetViewModelPageType());
+        }
+
+
+
+    }
+
+    public class DummyPageService : IPageService
+    {
+        public Type GetViewModelPageType()
+        {
+            return _viewModel.GetType();
+        }
+
+        public async Task GoNext(Type viewModelType)
+        {
+            throw new NotImplementedException();
+        }
+
+        private object _viewModel;
+
+        public async Task GoNext(object viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+      
     }
 }
